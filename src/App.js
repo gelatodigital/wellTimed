8d43a586322ff85a004bcd7d18509774d9df11d9
Hilmar Orth <hm.orth@aol.com>
2019-10-19 11:48:11 +0200

    init react, fetched contracts abis, set web3-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import Web3Provider from 'web3-react'
+import InjectedConnector from './InjectedConnector'
+import NetworkOnlyConnector from './NetworkOnlyConnector'
 import './App.css';
+import MyComponent from './components/MyComponent'
+// const dotenv = require('dotenv');
+// const env = dotenv.config().parsed
+
+// const { InjectedConnector, NetworkOnlyConnector } = Connectors;
+const Infura = new NetworkOnlyConnector({ providerURL: process.env.REACT_APP_NETWORK_URL })
+const MetaMask = new InjectedConnector({ supportedNetworks: [1, 4] })
+const connectors = {MetaMask, Infura}
+
+
+// `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Web3Provider connectors={connectors} libraryName={"ethers.js"}>
+          <div className="App">
+            <MyComponent />
+          </div>
+      </Web3Provider>
+
   );
 }
 
---
+++ src/constants/contractAddresses.js
@@ -0,0 +1,10 @@
+export const addresses = {
+    'dummy': "0xf1c13889924609f102D359Ca41ABaB9AEe072C77",
+    'ds-proxy-registry': '0xdd437ccFE6850577486A85E1CD1DC801F73D95de',
+    'ds-guard-factory': '0x01557De133751627DecaC658c998F9aE3384c797',
+    'ds-proxy-factory': '0x07d198bb76d0be34FCcD20233Bcfb94b21445f4d',
+    'ds-proxy-example': '0xFe702693D7fa5d944bA3E19C8ED9Bb25876E3393',
+    'ds-guard-example': '0x60FDFD25c8aC101375E1B1E855cA34358e4d7794'
+
+
+}
