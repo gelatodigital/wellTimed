189d8c6df20ac9f8d44fd5032c7cb4315294adb6
Hilmar Orth <hm.orth@aol.com>
2019-10-28 20:20:08 +0100

    updated dependencies

---
+++ build/precache-manifest.23ead68eeffaa5dd31cf807806a00208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e488260ec5bff9c3cce5ae75b573e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e5c01ecc109b4ce30e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9dac961256f3b554dc65",
+    "url": "/static/js/2.ed331c9b.chunk.js"
+  },
+  {
+    "revision": "e8e5c01ecc109b4ce30e",
+    "url": "/static/js/main.3a4e5e2e.chunk.js"
+  },
+  {
+    "revision": "9c7938078207effb84f8",
+    "url": "/static/js/runtime-main.a30d04b7.js"
+  }
+]);
